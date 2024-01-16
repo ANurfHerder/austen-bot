554743e62053d5802b768a973da0a0415510b23c
ANurfHerder <136250209+ANurfHerder@users.noreply.github.com>
2024-01-16 11:43:46 -0500

    added basic js typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateReply(event) {
+  event.preventDefault();
 
+  new Typewriter("#reply", {
+    strings: ["my dearest"],
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let replyFormElement = document.querySelector("#reply-generator");
+replyFormElement.addEventListener("submit", generateReply);
