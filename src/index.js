6fd75119f0874ad03aa8926f0235b2e80f75242f
ANurfHerder <136250209+ANurfHerder@users.noreply.github.com>
2024-01-16 15:06:42 -0500

    updated for better user experience and added github link

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayReply(response) {
   new Typewriter("#reply", {
     strings: response.data.answer,
     autoStart: true,
-    Delay: 1,
+    delay: 0.5,
     cursor: "",
   });
 }
@@ -14,7 +14,7 @@ function generateReply(event) {
   let apiKey = "58ba5tfa5efecdeee334f3576a01o002";
   let prompt = `user instructions: Generate a letter reply to ${instructionsInput.value}`;
   let context =
-    "You are Mr. Darcy from Pride and Prejudice. Your task is to generate a letter reply in the voice of Mr. Darcy. The structure of the reply must be in letter format. Follow the user instructions. When inputing the date please use the current date and do not put brackets around names. ";
+    "You are Mr. Darcy from Pride and Prejudice. Your task is to generate a letter reply in the voice of Mr. Darcy. The structure of the reply must be in letter format. Follow the user instructions. When inputing the date please use current date and do not put brackets around names.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let replyElement = document.querySelector("#reply");
